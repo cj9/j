54a51b5a562eaef8f956c8b32e1924e5dd2b1f3e
gt <gutaoai@126.com>
2016-03-01 00:04:20 +0800

    update RegisterUser model

--- src/main/java/com/gt/model/RegisterUser.java
+++ src/main/java/com/gt/model/RegisterUser.java
@@ -1,6 +1,18 @@
 package com.gt.model;
 
 public class RegisterUser {
-    String  id;
+    String id;
     String email;
+    String displayName;
+    String password;
+    String confirmPassword;
+    String captchaKey;
+    String captchaValue;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+    
 }
